85fa28c157b419540b8bf0cb3345bf63ac8280c9
ardailhan <ardailhan_@outlook.com>
2023-05-04 19:54:22 +0300

    Add project files.

---
+++ src/app/admin/components/orders/orders.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
+
+@Component({
+  selector: 'app-orders',
+  templateUrl: './orders.component.html',
+  styleUrls: ['./orders.component.scss']
+})
+export class OrdersComponent extends BaseComponent {
+
+constructor(spinner : NgxSpinnerService) {
+  super(spinner);
+}
+
+ngOnInit(): void{
+  this.showSpinner(SpinnerType.BallAtom)
+}
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AdminModule } from './admin/admin.module';
+import { UiModule } from './ui/ui.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { HttpClientModule } from '@angular/common/http';
+import { DeleteDirective } from './directives/admin/delete.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DeleteDirective
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AdminModule, UiModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    NgxSpinnerModule,
+    HttpClientModule
+  ],
+  providers: [
+    {provide: "baseUrl", useValue: "https://localhost:7297/api", multi: true}
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
