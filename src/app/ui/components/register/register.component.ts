10aed376e0493b9d2b1ae8a1001b402d66618ad1
ardailhan <ardailhan_@outlook.com>
2023-05-10 13:34:46 +0300

    configurations with user registration using reactive forms

--- src/app/ui/components/register/register.component.ts
+++ src/app/ui/components/register/register.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import { User } from '../../../entities/user';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  constructor(private formBuilder: FormBuilder) { }
 
+
+  frm: FormGroup;
+  ngOnInit(): void {
+    this.frm = this.formBuilder.group({
+      nameSurname: ["", [Validators.required,
+      Validators.maxLength(50),
+      Validators.minLength(3)]],
+      userName: ["", [Validators.required,
+      Validators.maxLength(50),
+      Validators.minLength(3)]],
+      email: ["", [Validators.required,
+      Validators.maxLength(250),
+      Validators.email]],
+      password: ["",
+        [
+          Validators.required
+        ]],
+      passwordConfirm: ["",
+        [
+          Validators.required
+        ]]
+    }, {
+      validators: (group: AbstractControl): ValidationErrors | null=> {
+        let password = group.get("password").value;
+        let passwordConfirm = group.get("passwordConfirm").value;
+        return password === passwordConfirm ? null : { notSame: true };
+      }
+    })
+  }
+
+  get component() {
+    return this.frm.controls;
+  }
+
+  submitted: boolean;
+  onSubmit(data: User) {
+    this.submitted = true;
+
+    if (this.frm.invalid)
+      return;
+  }
 }
