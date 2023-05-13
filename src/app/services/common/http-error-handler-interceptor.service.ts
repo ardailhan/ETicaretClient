1f278b37d4f5d0cd08655d43b44259023ff4b410
ardailhan <ardailhan_@outlook.com>
2023-05-13 22:09:46 +0300

    Http Interceptor with global http error handler

---
+++ src/app/services/common/http-error-handler-interceptor.service.ts
@@ -0,0 +1,51 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpStatusCode } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, Observable, of } from 'rxjs';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from '../ui/custom-toastr.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpErrorHandlerInterceptorService implements HttpInterceptor {
+
+  constructor(private toastrService : CustomToastrService) { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+    return next.handle(req).pipe(catchError(error => {
+      switch (error.status) {
+        case HttpStatusCode.Unauthorized:
+          this.toastrService.message("You are not authorized for this area!", "Unauthorized Action!", {
+            messageType: ToastrMessageType.Warning,
+            position: ToastrPosition.BottomFullWidth
+          });
+          break;
+        case HttpStatusCode.InternalServerError:
+          this.toastrService.message("Can Not Access Server!!!", "Server Error", {
+            messageType: ToastrMessageType.Warning,
+            position: ToastrPosition.BottomFullWidth
+          });
+          break;
+        case HttpStatusCode.BadRequest:
+          this.toastrService.message("Invalid Request Made !!!", "Invalid Request!", {
+            messageType: ToastrMessageType.Warning,
+            position: ToastrPosition.BottomFullWidth
+          });
+          break;
+        case HttpStatusCode.NotFound:
+          this.toastrService.message("Page Not Found", "Page Not Found!", {
+            messageType: ToastrMessageType.Warning,
+            position: ToastrPosition.BottomFullWidth
+          });
+          break;
+        default:
+          this.toastrService.message("Unexpected Error Occured!", "Unexpected Error!", {
+            messageType: ToastrMessageType.Warning,
+            position: ToastrPosition.BottomFullWidth
+          });
+          break;
+      }
+      return of(error);
+    }));
+  }
+}
