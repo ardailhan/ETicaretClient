4c059820219cb65c00b2842bede997bcbb13e41f
ardailhan <ardailhan_@outlook.com>
2023-05-15 23:40:31 +0300

    signalR angular side ..

--- src/app/admin/components/dashboard/dashboard.component.ts
+++ src/app/admin/components/dashboard/dashboard.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
 import { AlertifyService, MessageType, Position } from 'src/app/services/admin/alertify.service';
+import { HubUrls } from '../../../constants/hub-urls';
+import { ReceiveFunctions } from '../../../constants/receive-functions';
+import { SignalRService } from '../../../services/common/signalr.service';
 
 
 @Component({
@@ -11,11 +14,18 @@ import { AlertifyService, MessageType, Position } from 'src/app/services/admin/a
 })
 export class DashboardComponent extends BaseComponent {
 
-  constructor(private alertify: AlertifyService, spinner : NgxSpinnerService) {
-    super(spinner);
+  constructor(private alertify: AlertifyService, spinner : NgxSpinnerService, private signalRService: SignalRService) {
+    super(spinner)
+    signalRService.start(HubUrls.ProductHub)
   }
 
   ngOnInit(): void {
+    this.signalRService.on(ReceiveFunctions.ProductAddedMessageReceiveFunction, message => {
+      this.alertify.message(message, {
+        messageType: MessageType.Notify,
+        position: Position.TopRight
+      })
+    });
   }
 
   m(){
