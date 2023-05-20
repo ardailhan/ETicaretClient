595c5615f32257ffdf31e3281ce56382e6b16870
ardailhan <ardailhan_@outlook.com>
2023-05-20 21:16:13 +0300

    password-reset need more work..

--- src/app/ui/components/components.module.ts
+++ src/app/ui/components/components.module.ts
@@ -4,18 +4,23 @@ import { ProductsModule } from './products/products.module';
 import { BasketsModule } from './baskets/baskets.module';
 import { HomeModule } from './home/home.module';
 import { RegisterModule } from './register/register.module';
-import { LoginModule } from './login/login.module';
+import { UpdatePasswordModule } from './update-password/update-password.module';
+import { PasswordResetModule } from './password-reset/password-reset.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+  
+  ],
   imports: [
     CommonModule,
     ProductsModule,
     BasketsModule,
     HomeModule,
     RegisterModule,
+    PasswordResetModule,
+    UpdatePasswordModule
     //LoginModule
   ],
   exports: [
