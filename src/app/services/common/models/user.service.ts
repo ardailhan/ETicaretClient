9ba4e416abd917f3ee6772bb2d14d136eb6267bc
ardailhan <ardailhan_@outlook.com>
2023-05-21 16:03:51 +0300

    password reset configs...

--- src/app/services/common/models/user.service.ts
+++ src/app/services/common/models/user.service.ts
@@ -1,7 +1,5 @@
-import { SocialUser } from '@abacritt/angularx-social-login';
 import { Injectable } from '@angular/core';
 import { firstValueFrom, Observable } from 'rxjs';
-import { TokenResponse } from '../../../contracts/token/tokenResponse';
 import { Create_User } from '../../../contracts/users/create_user';
 import { User } from '../../../entities/user';
 import { CustomToastrService, ToastrMessageType, ToastrPosition } from '../../ui/custom-toastr.service';
@@ -22,5 +20,19 @@ export class UserService {
     return await firstValueFrom(observable) as Create_User;
   }
 
-  
+  async updatePassword(userId: string, resetToken: string, password: string, passwordConfirm: string, successCallBack?: () => void, errorCallBack?: (error) => void) {
+    const observable: Observable<any> = await this.httpClientService.post({
+      action: "update-password",
+      controller: "user"
+    }, {
+      userId: userId,
+      resetToken: resetToken,
+      password: password,
+      passwordConfirm: passwordConfirm
+    });
+
+    const promiseData: Promise<any> = firstValueFrom(observable);
+    promiseData.then(value => successCallBack()).catch(error => errorCallBack(error));
+    await promiseData;
+  }
 }
--- src/app/ui/components/password-reset/password-reset.component.ts
+++ src/app/ui/components/password-reset/password-reset.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { BaseComponent, SpinnerType } from '../../../base/base.component';
+import { AlertifyService, MessageType, Position } from '../../../services/admin/alertify.service';
+import { UserAuthService } from '../../../services/common/models/user-auth.service';
 
 @Component({
   selector: 'app-password-reset',
   templateUrl: './password-reset.component.html',
   styleUrls: ['./password-reset.component.scss']
 })
-export class PasswordResetComponent {
+export class PasswordResetComponent extends BaseComponent {
+  constructor(spinner: NgxSpinnerService, private userAuthService: UserAuthService, private alertifyService: AlertifyService) {
+    super(spinner)
+  }
 
+  passwordReset(email: string) {
+    this.showSpinner(SpinnerType.BallAtom)
+    this.userAuthService.passwordReset(email, () => {
+      this.hideSpinner(SpinnerType.BallAtom)
+      this.alertifyService.message("Mail sended successfully!", {
+        messageType: MessageType.Notify,
+        position: Position.TopRight
+      })
+    });
+  }
 }
