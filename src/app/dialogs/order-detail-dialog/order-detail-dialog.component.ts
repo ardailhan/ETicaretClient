b248b2cf8f0e2f7e4b3aee86555e9c66f91996b4
ardailhan <ardailhan_@outlook.com>
2023-05-21 22:18:55 +0300

    complete order work done

--- src/app/dialogs/order-detail-dialog/order-detail-dialog.component.ts
+++ src/app/dialogs/order-detail-dialog/order-detail-dialog.component.ts
@@ -1,8 +1,14 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { async } from 'rxjs';
+import { SpinnerType } from '../../base/base.component';
 import { SingleOrder } from '../../contracts/order/single_order';
+import { DialogService } from '../../services/common/dialog.service';
 import { OrderService } from '../../services/common/models/order.service';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from '../../services/ui/custom-toastr.service';
 import { BaseDialog } from '../base/base-dialog';
+import { CompleteOrderDialogComponent, CompleteOrderState } from '../complete-order-dialog/complete-order-dialog.component';
 
 @Component({
   selector: 'app-order-detail-dialog',
@@ -12,7 +18,7 @@ import { BaseDialog } from '../base/base-dialog';
 export class OrderDetailDialogComponent extends BaseDialog<OrderDetailDialogComponent> implements OnInit {
   constructor(
     dialogRef: MatDialogRef<OrderDetailDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: OrderDetailDialogState | string, private orderService: OrderService) {
+    @Inject(MAT_DIALOG_DATA) public data: OrderDetailDialogState | string, private orderService: OrderService, private dialogService: DialogService, private spinner: NgxSpinnerService,private toastrService: CustomToastrService) {
     super(dialogRef)
   }
 
@@ -28,6 +34,22 @@ export class OrderDetailDialogComponent extends BaseDialog<OrderDetailDialogComp
     this.dataSource = this.singleOrder.basketItems;
 
     this.totalPrice = this.singleOrder.basketItems.map((basketItem, index) => basketItem.price * basketItem.quantity).reduce((price, current) => price + current);
+  }
+
+  completeOrder() {
+    this.dialogService.openDialog({
+      componentType: CompleteOrderDialogComponent,
+      data: CompleteOrderState.Yes,
+      afterClosed: async () => {
+        this.spinner.show(SpinnerType.BallAtom);
+        await this.orderService.completeOrder(this.data as string);
+        this.spinner.hide(SpinnerType.BallAtom);
+        this.toastrService.message("Order Successfully Created! Customer informed", "Order Completed !", {
+          messageType: ToastrMessageType.Success,
+          position: ToastrPosition.TopRight
+        });
+      }
+    });
   }
 }
 
