6d5e40be18f4babb59b1389e9c1836af92e60e7f
ardailhan <ardailhan_@outlook.com>
2023-05-24 15:00:31 +0300

    authorization menu updates

--- src/app/dialogs/authorize-menu-dialog/authorize-menu-dialog.component.ts
+++ src/app/dialogs/authorize-menu-dialog/authorize-menu-dialog.component.ts
@@ -16,33 +16,40 @@ import { BaseDialog } from '../base/base-dialog';
   styleUrls: ['./authorize-menu-dialog.component.scss']
 })
 export class AuthorizeMenuDialogComponent extends BaseDialog<AuthorizeMenuDialogComponent> implements OnInit {
-  constructor(
-    dialogRef: MatDialogRef<AuthorizeMenuDialogComponent>,
+  constructor(dialogRef: MatDialogRef<AuthorizeMenuDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
     private roleService: RoleService,
     private authorizationEndpointService: AuthorizationEndpointService,
-  private spinner: NgxSpinnerService) {
+    private spinner: NgxSpinnerService) {
     super(dialogRef)
   }
-
   roles: { datas: List_Role[], totalCount: number };
-
+  assignedRoles: Array<string>;
+  listRoles: { name: string, selected: boolean }[];
   async ngOnInit() {
+    this.assignedRoles = await this.authorizationEndpointService.getRolesToEndpoint(this.data.code, this.data.menuName);
+
     this.roles = await this.roleService.getRoles(-1, -1);
+
+    this.listRoles = this.roles.datas.map((r: any) => {
+      return {
+        name: r.name,
+        selected: this.assignedRoles?.indexOf(r.name) > -1
+      }
+    });
   }
 
   assignRoles(rolesComponent: MatSelectionList) {
     const roles: string[] = rolesComponent.selectedOptions.selected.map(o => o._elementRef.nativeElement.innerText)
     this.spinner.show(SpinnerType.BallAtom);
-    this.authorizationEndpointService.assignRoleEndpoint(roles, this.data.code, this.data.menuName, () => {
-      this.spinner.hide(SpinnerType.BallAtom);
-    }, error => {
+    this.authorizationEndpointService.assignRoleEndpoint(roles, this.data.code, this.data.menuName,
+      () => {
+        this.spinner.hide(SpinnerType.BallAtom);
+      }, error => {
 
-    })
+      })
   }
-
 }
-
 
 export enum AuthorizeMenuState {
   Yes,
--- src/app/services/common/models/authorization-endpoint.service.ts
+++ src/app/services/common/models/authorization-endpoint.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { firstValueFrom, Observable } from 'rxjs';
 import { HttpClientService } from '../http-client.service';
 
 @Injectable({
@@ -24,5 +24,20 @@ export class AuthorizationEndpointService {
     });
 
     await promiseData;
+  }
+
+  async getRolesToEndpoint(code: string,menu: string, successCallBack?: () => void, errorCallBack?: (error) => void):Promise<string[]> {
+    const observable: Observable<any> = this.httpClientService.post({
+      controller: "AuthorizationEndpoints",
+      action: "GetRolesToEndpoint"
+    }, {
+      code: code,
+      menu: menu
+    });
+
+    const promiseData = firstValueFrom(observable);
+    promiseData.then(successCallBack).catch(errorCallBack);
+    
+    return (await promiseData).roles;
   }
 }
